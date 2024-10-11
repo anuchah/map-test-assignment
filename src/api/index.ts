7a46b445d5183bcf1621c4e841db98caed317ae7
anuchah <anucha.ng@kkumail.com>
2024-10-11 10:44:30 +0700

    fix url

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
-import {API_URL} from '../constants/api';
 
 export const api = axios.create({
-  baseURL: API_URL,
+  baseURL: 'https://v2k-dev.vallarismaps.com/core/api',
   timeout: 30000,
 });
